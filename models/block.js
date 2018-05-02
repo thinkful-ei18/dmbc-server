ccf1a56738b82347962daafb843772a16861c7bf
Dylan <nielsen.dylan@gmail.com>
2018-05-02 08:43:41 -0700

    Add block schema/endpoint/tests

---
+++ models/block.js
@@ -0,0 +1,20 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const blockSchema = mongoose.Schema({
+  cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Card' }],
+  date: { type: Date, required: true },
+  title: { type: String, required: true }
+});
+
+blockSchema.set('toObject', {
+  transform: function(doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+const Block = mongoose.model('Block', blockSchema);
+
+module.exports = { Block };
