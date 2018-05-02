02e3c5e663258373f452a4ab3f28081020de0747
Dylan <nielsen.dylan@gmail.com>
2018-05-02 13:56:51 -0700

    Add DB seed file

---
+++ utils/seed-db.js
@@ -0,0 +1,24 @@
+'use strict';
+const mongoose = require('mongoose');
+const { User } = require('../models/user');
+const { Itinerary } = require('../models/itinerary');
+const { Block } = require('../models/block');
+const seedItineraries = require('../db/seed/itineraries');
+const seedUsers = require('../db/seed/users');
+const seedBlocks = require('../db/seed/blocks');
+const { DATABASE_URL } = require('../config');
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => mongoose.connection.db.dropDatabase())
+  .then(() => User.insertMany(seedUsers))
+  .then(() => Itinerary.insertMany(seedItineraries))
+  .then(() => Block.insertMany(seedBlocks))
+  .then(() => User.ensureIndexes())
+  .then(() => Itinerary.ensureIndexes())
+  .then(() => Block.ensureIndexes())
+  .then(() => mongoose.disconnect())
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
