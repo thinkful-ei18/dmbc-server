fc7bbaff7122b17bd6581e2299a0c8668fce9340
Dylan <nielsen.dylan@gmail.com>
2018-05-04 09:14:03 -0700

    Populate destinations and fix seed-file

--- utils/seed-db.js
+++ utils/seed-db.js
@@ -5,10 +5,12 @@ const { User } = require('../models/user');
 const { Itinerary } = require('../models/itinerary');
 const { Block } = require('../models/block');
 const { Card } = require('../models/card');
+const { Destination } = require('../models/destination');
 const seedItineraries = require('../db/seed/itineraries');
 const seedUsers = require('../db/seed/users');
 const seedBlocks = require('../db/seed/blocks');
 const seedCards = require('../db/seed/cards');
+const seedDestinations = require('../db/seed/destination');
 const { DATABASE_URL } = require('../config');
 
 mongoose
@@ -18,10 +20,12 @@ mongoose
   .then(() => Itinerary.insertMany(seedItineraries))
   .then(() => Block.insertMany(seedBlocks))
   .then(() => Card.insertMany(seedCards))
+  .then(() => Destination.insertMany(seedDestinations))
   .then(() => User.ensureIndexes())
   .then(() => Itinerary.ensureIndexes())
   .then(() => Block.ensureIndexes())
   .then(() => Card.ensureIndexes())
+  .then(() => Destination.ensureIndexes())
   .then(() => mongoose.disconnect())
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
